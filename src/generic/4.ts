9840fcfac52a8f4b11941e61ac0b4823a26fc342
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-02-13 19:19:25 +0200

    update

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,21 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface ComponentProps {
+  title: string;
+}
 
-class Component {
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+interface PageProps extends ComponentProps {
+  title: string;
+}
+
+class Page extends Component<ComponentProps>{
   pageInfo () {
     console.log(this.props.title);
   }
